cce530f5d6efe7a1f0837481d182026a5305174a
ranerane0101 <runrunrun4649@icloud.com>
2021-10-25 17:58:40 +0900

    second commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'MDCアドベントカレンダー',
+    themeConfig: {
+        sidebar: [
+            '/',
+            '/article_1',
+            '/article_2',
+            '/article_3',
+        ],
+    },
+};
